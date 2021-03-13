243a7f2f67139f5452308c516602e886249824d7
이광현 <rhkd4129@naver.com>
2021-03-13 21:58:50 +0900

    Create 계산기.java

---
+++ 계산기.java
@@ -0,0 +1,38 @@
+package classpart;
+
+public class Calculator {
+
+		int left, right;
+		
+		public void setOprands(int left, int right) {
+			this.left = left;
+			this.right = right;
+		}
+		
+		public void sum() {
+			System.out.println(this.left+this.right);
+		}
+		
+		
+		public void avg() {
+			System.out.println((this.left+this.right)/2);
+		}
+	}
+
+
+public class PersonTest {
+
+
+	public static void main(String[] args) { 
+		
+		Calculator c1 = new Calculator();
+		c1.setOprands(10,20);
+		c1.sum();
+		c1.avg();
+		
+	
+		
+
+	
+	}
+}
